77315754afb00daf8dfd87bcbe22b1f5db620989
Abdullah-ahfj <abdullahahfj@gmail.com>
2025-03-31 17:55:25 +0530

    App.js changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Technologies from './components/Technologies';
-import Projects from './components/projects';
+import Projects from './components/Projects.';
 import Education from './components/Education';
 import Contact from './components/Contact';
 
